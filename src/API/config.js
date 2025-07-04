0447be7f3adfb246a21ccfa52cfbe6ab9e80ae77
mohamedabdelhameid <mohameedabdelhameed8879@gmail.com>
2025-07-05 02:32:15 +0300

    changes

--- src/API/config.js
+++ src/API/config.js
@@ -1,3 +1,8 @@
-const BASE_BACKEND_URL = 'http://localhost:8000/api';
+import BASE_BACKEND_LOCAHOST_URL from './localhost';
+
+
+// const BASE_BACKEND_URL = 'http://localhost:8000/api';
+const BASE_BACKEND_URL = `${BASE_BACKEND_LOCAHOST_URL}/api`;
+
 
 export default BASE_BACKEND_URL;
